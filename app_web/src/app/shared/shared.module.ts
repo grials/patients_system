134e6064d635fdd19ef64e43e5e32f140e39a797
Grials <joseudemy@gmail.com>
2022-02-05 21:42:26 -0400

    firts stage page home

--- app_web/src/app/shared/shared.module.ts
+++ app_web/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent],
+  declarations: [FooterComponent, HeaderComponent, SkillsComponent],
   imports: [CommonModule, RouterModule],
-  exports: [FooterComponent, HeaderComponent],
+  exports: [FooterComponent, HeaderComponent, SkillsComponent],
 })
 export class SharedModule {}
